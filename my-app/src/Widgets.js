09ce83805b65d0280d9e0dd6cbf9152e71790129
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-01-27 16:42:33 +0530

    widgets sections  completed

--- my-app/src/Widgets.js
+++ my-app/src/Widgets.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import './Widgets.css'
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  TwitterTimelineEmbed,
+  TwitterSharebutton,
+  TwitterTweetEmbed,
+  TwitterShareButton,
+} from "react-twitter-embed";
 function Widgets(){
   return ( 
   <div className="widgets">
-   <h2>Widgets</h2> 
+        <div className="widgets_input">
+          <SearchIcon className="widgets_searchIcon"/>
+          <input placeholder="Search twitter" type="text"/>
+        </div>
+        <div className="widgets_widgetContainer">
+          <h2>Whats happening</h2>
+          <TwitterTweetEmbed tweetId={"1341072021099327489"} />
+           
+           <TwitterTimelineEmbed  
+            souceType="profile"
+            screenName="reactjs"
+            options={{height:400}}
+           />
+           <TwitterShareButton
+               url={"https://facebook.com/cleverprogrammer"}
+               options={{ text: "#reactjs is awesome", via: "Ritishjaiswal" }}
+           />
+          </div>
   </div>
   );
 }
