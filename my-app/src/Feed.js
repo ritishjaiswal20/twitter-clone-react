d8aefe8452de92c4caf8e778e39084ec6c36be8b
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-01-28 10:28:17 +0530

    connecting with firebase

--- my-app/src/Feed.js
+++ my-app/src/Feed.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.css'
 import TweetBox from './TweetBox.js'
 import Post from './Post.js'
+import db from './firebase'
 function Feed() {
+  const[posts,setPosts]=useState([]);
+  
+  useEffect(()=>{
+    db.collection('posts').onSnapshot(snapshot =>(
+      setPosts(snapshot.docs.map(doc =>doc.data()))
+    ))
+  },[]);
   return (
   <div className="feed">
     <div className="feed_header">
-      <h2>Home</h2>
+      <h2>Home</h2>  
     </div>
     <TweetBox/>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
+
+    {posts.map(post=>(
+    <Post 
+    
+     displayName={post.displayName}
+     username={post.username}
+     verified={post.verified}
+     text={post.text}
+     avatar={post.avatar}
+     image={post.image}
+
+    />
+    ))}
+  
   </div>
   );
 }
